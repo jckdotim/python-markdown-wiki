8c2705c5641e51e2618c687e591f0a78d443b4e4
jc <shinvee@spoqa.com>
2015-10-23 14:53:51 +0900

    Add Topic.find() test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,13 @@
-from app import parse_link
+from app import parse_link, Topic
 
 
 def test_parse_link():
     assert parse_link(u'[[Topic]]') == u'[Topic](/Topic)'
+
+
+def test_topic_find(fx_session):
+    topic = Topic.find('test')
+    fx_session.add(topic)
+    fx_session.commit()
+    topic2 = Topic.find('test')
+    assert topic.created_at == topic2.created_at
