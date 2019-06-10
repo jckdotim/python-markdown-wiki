2ff7115a32416234cbbae1c45b399c9575bde28f
JC Kim <jck@linecorp.com>
2019-06-10 22:23:42 +0900

    refactor: shorten cli code

--- cli.py
+++ cli.py
@@ -7,18 +7,14 @@ def cli():
     pass
 
 
-@click.command()
+@cli.command()
 def createdb():
     db.create_all()
 
 
-@click.command()
+@cli.command()
 def dropdb():
     db.drop_all()
-
-
-cli.add_command(createdb)
-cli.add_command(dropdb)
 
 
 if __name__ == '__main__':
