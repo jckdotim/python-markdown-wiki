6e8fcc55ea0b40bce6d171120938e6f654126836
JC Kim <jckdotim@gmail.com>
2020-04-09 21:57:10 +0900

    style: 💄 use github-markdown css

--- entry.js
+++ entry.js
@@ -1,6 +1,7 @@
 var $ = require('jquery')
 require('bootstrap')
 require('bootstrap/dist/css/bootstrap.css')
+require('github-markdown-css')
 require('inline-attachment/src/inline-attachment')
 require('inline-attachment/src/jquery.inline-attachment')
 
